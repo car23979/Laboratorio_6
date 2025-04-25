0b94a05f848ea3bd9c3232e6a7aaab10789d39e5
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-25 15:32:25 -0600

    Difiniciones
    Definiciones y librerias

--- Laboratorio6/Laboratorio6/main.c
+++ Laboratorio6/Laboratorio6/main.c
@@ -4,3 +4,11 @@
  * Created: 25/04/2025 12:08:10 p. m.
  * Author : David Carranza
  */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
