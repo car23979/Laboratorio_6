f3df1a00d3a90a4add22d3c259f4fcde54de994d
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-25 15:41:10 -0600

    Buffer
    Se espera a que el buffer este vacio con el while

--- Laboratorio6/Laboratorio6/main.c
+++ Laboratorio6/Laboratorio6/main.c
@@ -11,11 +11,16 @@
 #include <util/delay.h>
 
 #define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1
+#define UBRR_VALUE ((F_CPU / (16UL * BAUD)) - 1)
 
 void UART_init(void) {
 	UBRR0H = (unsigned char)(UBRR_VALUE >> 8);
 	UBRR0L = (unsigned char)(UBRR_VALUE);
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);  // RX y TX habilitados
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 bits, sin paridad
+}
+
+void UART_sendChar(char data) {
+	while (!(UCSR0A & (1 << UDRE0)));  // Espera a que el buffer esté vacío
+	UDR0 = data;
 }
